bf9b1c5908e47d1d1acb249191e75be3ae0b11de
Eric R <62493059+EricERodriguez@users.noreply.github.com>
2021-04-21 02:11:29 -0300

    Add files via upload

---
+++ src/Logica/main.java
@@ -0,0 +1,15 @@
+package Logica;
+
+import IGU.Principal;
+
+public class main 
+{
+
+    public static void main(String[] args) 
+    {
+        Principal princi = new Principal();
+        princi.setVisible(true);
+        princi.setLocationRelativeTo(null);
+    }
+    
+}
